53572c79be1b149db8786ad48dec327e5d2ba67e
umamaheswari0312 <umamaheswari.attada@gmail.com>
2025-01-09 21:28:12 +0530

    Added idly

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test 
 repo for basic git commands demonstartion
+print("Idly")
--- hungry.py
+++ hungry.py
@@ -1,5 +1,6 @@
 hungry=input("Are you hungry?")
 if hungry=="yes":
     print("Eat something")
+    print("Idly")
 else:
     print("Do your homework")
