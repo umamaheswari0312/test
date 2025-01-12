6088a1360e8578f12160d4b237417ae7d199ba8c
umamaheswari0312 <umamaheswari.attada@gmail.com>
2025-01-12 10:58:24 +0530

    reset hungry

--- hungry.py
+++ hungry.py
@@ -1,22 +1,8 @@
 hungry=input("Are you hungry?")
 if hungry=="yes":
     print("Eat something")
-    print("Idly")
-    print("sambar")
-    print("coconut chutney")
-    print("Allam chutney")
-    print("Karappodi")
-    print("not interested")
-    print("ghee")
-    print("modifying")
-
 else:
-    thirsty=input("Are you thirsty?")
-    if thirsty=="yes":
-        print("drink water")
-        print("drink jira water")
-        print("drink lemon water")
-        print("no water")
-    else:
-        print("Do your homework")
+    print("Do your homework")
+
+
 
